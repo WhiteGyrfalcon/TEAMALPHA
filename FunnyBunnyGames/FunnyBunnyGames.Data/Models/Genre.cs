fe47021d79aedded50c40184213cf959d457d0c5
LuchezarAtanasov17 <114495467+LuchezarAtanasov17@users.noreply.github.com>
2023-10-26 08:27:20 +0300

    Add entity models.

---
+++ FunnyBunnyGames/FunnyBunnyGames.Data/Models/Genre.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FunnyBunnyGames.Data.Models
+{
+    public class Genre
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<Game> Games  { get; set; }
+    }
+}
---
+++ FunnyBunnyGames/FunnyBunnyGames.Data/Models/Rating.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FunnyBunnyGames.Data.Models
+{
+    public class Rating
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public Guid GameId { get; set; }
+
+        public double Value { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public User User { get; set; }
+
+        [ForeignKey(nameof(GameId))]
+        public Game Game { get; set; }
+    }
+}
