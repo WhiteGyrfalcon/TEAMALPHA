4b199c1b520a373a0382cc8479019cd38ed929f5
Ananasa05 <ananasacarpet@gmail.com>
2024-01-18 10:18:56 +0200

    Add services for rating games.

---
+++ FunnyBunnyGames/FunnyBunnyGames.Services/Ratings/IRatingService.cs
@@ -0,0 +1,11 @@
+﻿using FunnyBunnyGames.Services.Games.ViewModel;
+
+namespace FunnyBunnyGames.Services.Ratings
+{
+    public interface IRatingService
+    {
+        Task UpdateRatingAsync(RatingsViewModel request);
+
+        Task<double> AvegareRatingAsync(Guid id);
+    }
+}
---
+++ FunnyBunnyGames/FunnyBunnyGames.Services/Ratings/RatingService.cs
@@ -0,0 +1,58 @@
+﻿using FunnyBunnyGames.Data.Models;
+using FunnyBunnyGames.Services.Games.ViewModel;
+using Microsoft.EntityFrameworkCore;
+using Web.FunnyBunnyGames.Data;
+
+namespace FunnyBunnyGames.Services.Ratings
+{
+    public class RatingService:IRatingService
+    {
+        private readonly ApplicationDbContext context;
+        public RatingService(ApplicationDbContext dbContext) { this.context = dbContext; }
+
+        public async Task<double> AvegareRatingAsync(Guid id)
+        {
+            var models = await this.context.Ratings.Where(x=>x.GameId==id)
+                .Select(s => new RatingsViewModel()
+                {
+                    Value = s.Value,
+                })
+                .ToListAsync();
+
+            double avg = 0;
+            foreach (var model in models)
+            {
+                avg += model.Value;
+            }
+
+            return avg/models.Count;
+        }
+
+        public async Task UpdateRatingAsync(RatingsViewModel request)
+        {
+            if (request is null)
+            {
+                throw new ArgumentNullException(nameof(request));
+            }
+
+            var entities = await context.Ratings
+                .Where(x => x.UserId == request.UserId && x.GameId == request.GameId)
+                .ToListAsync();
+
+            context.Ratings.RemoveRange(entities);
+
+            var rating = new Rating()
+            {
+                GameId = request.GameId,
+                Value = request.Value,
+                UserId = request.UserId,
+            };
+
+            await context.AddAsync(rating);
+
+            await context.SaveChangesAsync();
+
+
+        }
+    }
+}
