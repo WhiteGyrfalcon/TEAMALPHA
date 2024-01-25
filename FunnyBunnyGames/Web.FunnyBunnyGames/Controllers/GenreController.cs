f9eb967cb427d57487f6c60ecffc06c29c70583b
Симона Палиева <simonapalieva05@pmgkk.com>
2024-01-25 10:05:19 +0200

    Created views for genre

---
+++ FunnyBunnyGames/Web.FunnyBunnyGames/Controllers/GenreController.cs
@@ -0,0 +1,46 @@
+﻿using FunnyBunnyGames.Services.Companies.ViewModels;
+using FunnyBunnyGames.Services.Genres;
+using FunnyBunnyGames.Services.Genres.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.FunnyBunnyGames.Controllers
+{
+    public class GenreController : Controller
+    {
+        private readonly IGenreService _genreService;
+
+        public GenreController(IGenreService genreService)
+        {
+            this._genreService = genreService;
+        }
+
+        public async Task<IActionResult> All()
+        {
+            var model = await _genreService.ListGenresAsync();
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> Delete(
+            [FromRoute]
+            Guid id)
+        {
+            await _genreService.DeleteGenreAsync(id);
+
+            return RedirectToAction("All", "Genre");
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(GenresViewModel model)
+        {
+            await _genreService.CreateGenreAsync(model);
+            return RedirectToAction("All", "Genre");
+        }
+    }
+}
